21460c6be30eec077c567bc31eb6b398883b7bf3
kazuzukan <banghaji363@gmail.com>
2022-01-19 14:46:12 +0700

    Fix: unusable comment line

--- main.go
+++ main.go
@@ -33,21 +33,3 @@ func main() {
 	// fmt.Println("Connection to database success")
 
 }
-
-// handler for gin or controller
-// func Handler(c *gin.Context) {
-// 	dsn := "root:@tcp(127.0.0.1:3306)/bwa-project?charset=utf8mb4&parseTime=True&loc=Local"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		// fatal untuk memberhentikan program kalau error
-// 		log.Fatal(err.Error())
-// 	}
-
-// 	fmt.Println("Connection to database success")
-
-// 	var users []user.User
-// 	db.Find(&users)
-
-// 	c.JSON(http.StatusOK, users)
-// }
